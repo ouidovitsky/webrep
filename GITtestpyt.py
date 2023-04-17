afae88d04239eb1155e702a0ba18a2a2078758e7
ouidovitsky <word.nba.a@gmail.com>
2023-04-17 00:16:21 +0000

    added a dict in python

---
+++ GITtestpyt.py
@@ -0,0 +1,14 @@
+
+
+dictedto = {
+    'harry' :'griffindoor',
+    'melfoy' : 'sledirin',
+    'hamadi' : 'zawiyt sheikh'
+    
+    }
+
+print(dictedto)
+
+dictedto['hermoyni'] = 'sledirin'
+
+print(dictedto)
